2adb0436f4f935c1bbb1c32158878497b3f124b4
abcvu <jack.nguyentruong@gmail.com>
2022-12-21 20:16:02 +0700

    Change blog template

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-	return (
-		<>
-			<Head>
-				<meta name="description" content="Developer Blog Offical Website" />
-				<meta name="keywords" content="IT,Blog,Nextjs,Website, An Nguyen" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="country" content="Viet Name" />
-				<link rel="icon" href="/favicon.ico" />
-				<title>An Nguyen Blog</title>
-			</Head>
-			<main className={styles.main}>
-        		<h1 className="text-center text-3xl font-bold underline">Hello, Welcome to An Nguyen Blog</h1>
-			</main>
-    	</>
-	)
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
+
